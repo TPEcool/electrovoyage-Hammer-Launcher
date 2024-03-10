80b9317f1feafdbdafec0f96467e359facbb642e
TPEcool <artemplay0607@gmail.com>
2024-03-10 11:48:33 +0300

    Make venv not a synced folder, fix virtual environment name

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 py -m PyInstaller --onefile -y --workpath temp/ --distpath "electrovoyage's Hammer Launcher" --upx-dir F:/upx --clean main.py
 py -m PyInstaller --onefile --workpath temp_startup/ --distpath "Startup program" --upx-dir F:/upx --clean --add-data "electrovoyage's Hammer Launcher/main.exe";"launcher" --add-data "startenv.bat";"launcher" startup.py
-py -m venv hammerlauncher
-move hammerlauncher "Startup program/venv" /Y
+py -m venv venv
+move venv "Startup program/venv" /Y
 rmdir /s /q "electrovoyage's Hammer Launcher"
