c443ac7925b67b8b8f2580433dac19b73d16c861
TPEcool <artemplay0607@gmail.com>
2024-03-10 11:51:38 +0300

    Fix venv folder not being moved properly

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 py -m PyInstaller --onefile -y --workpath temp/ --distpath "electrovoyage's Hammer Launcher" --upx-dir F:/upx --clean main.py
 py -m PyInstaller --onefile --workpath temp_startup/ --distpath "Startup program" --upx-dir F:/upx --clean --add-data "electrovoyage's Hammer Launcher/main.exe";"launcher" --add-data "startenv.bat";"launcher" startup.py
 py -m venv venv
-move venv "Startup program/venv" /Y
+move /Y venv "Startup program/venv"
 rmdir /s /q "electrovoyage's Hammer Launcher"
