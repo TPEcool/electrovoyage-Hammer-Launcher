9496f4d6b716e342225eaaca1f36ad88f502bc76
electrovoyage <75689188+TPEcool@users.noreply.github.com>
2024-05-14 19:11:12 +0300

    modified:   .gitignore 	modified:   launcher.py 	renamed:    logo.png -> resources/logo.png 	modified:   richpresence.py

--- launcher.py
+++ launcher.py
@@ -1,19 +1,25 @@
-print('electrovoyage\'s Hammer Launcher')
-
-import venv
-from os.path import dirname, join
-from os import system, getcwd as _getcwd, startfile, listdir
+from ttkbootstrap import *
+from srctools import keyvalues
+import os
+from richpresence import main as presence, stop as stoppresence
+import threading
 
 #system('rmdir /s /q hammerlauncher > nul')
 
 # required to use files written into the executable
 def getcwd() -> str:
-    return dirname(__file__)
+    return os.path.dirname(__file__)
 
-#print('Creating virtual environment...')
+'''#print('Creating virtual environment...')
 #venv.main(['hammerlauncher', '--system-site-packages'])
 
 print('Launching Rich Presence...')
 #print(listdir(getcwd()), listdir(getcwd() + '/launcher'))
 #system(join(getcwd(), 'launcher', f'startenv.bat "{getcwd()}" "{_getcwd()}"'))
-system(f'call "{_getcwd()}\hammerlauncher\Scripts\Activate.bat" && "{getcwd()}\launcher\\richpresence.exe"')
+system(f'call "{_getcwd()}\hammerlauncher\Scripts\Activate.bat" && "{getcwd()}\launcher\\richpresence.exe"')'''
+
+win = Window('electrovoyage\'s Hammer Launcher', 'litera', os.path.join(getcwd(), 'resources/logo.png'))
+presencethread = threading.Thread(target=presence)
+presencethread.start()
+
+win.mainloop()
--- richpresence.py
+++ richpresence.py
@@ -3,95 +3,109 @@ import pygetwindow as gw
 from pypresence import Presence, DiscordNotFound
 import time
 
-old_hammername = ''
-hammer_name = ''
-starttime = time.time()
+global shouldend
+shouldend = False
 
-def sliceSplit(s: str, delimiter: str, start: int, end: int) -> str:
-    '''
-Slice a split string from `start` to `end - 1`.
-    '''
-    return delimiter.join(s.split(delimiter)[start:end])
+def main():
+    global shouldend
+    old_hammername = ''
+    hammer_name = ''
+    starttime = time.time()
 
-def getViewportIndependentWindowName(name: str) -> str:
-    return sliceSplit(name, '-', 0, -1)
+    def sliceSplit(s: str, delimiter: str, start: int, end: int) -> str:
+        '''
+    Slice a split string from `start` to `end - 1`.
+        '''
+        return delimiter.join(s.split(delimiter)[start:end])
 
-print('Setting up rich presence:')
-try:
-    rpc = Presence('1216087154581573803', pipe=0)
-    print('\t1/3 - Rich Presence set up')
-    rpc.connect()
-    print('\t2/3 - Rich Presence connected')
-    rpc.update(
-        details='Idling',
-        state='electrovoyage\'s Hammer Launcher is open',
-        start = time.time(),
-        large_image='hammerlauncher',
-        large_text='electrovoyage\'s Hammer Launcher'
-    )
-    print('\t3/3 - Rich Presence set')
-except DiscordNotFound:
-    print('Discord not found, failed to start Rich Presence')
+    def getViewportIndependentWindowName(name: str) -> str:
+        return sliceSplit(name, '-', 0, -1)
 
-FOUND, IDLE, MISSING, SEARCHING = range(4)
-starttimes = {}
+    print('Setting up rich presence:')
+    try:
+        rpc = Presence('1216087154581573803', pipe=0)
+        print('\t1/3 - Rich Presence set up')
+        rpc.connect()
+        print('\t2/3 - Rich Presence connected')
+        rpc.update(
+            details='Idling',
+            state='electrovoyage\'s Hammer Launcher is open',
+            start = time.time(),
+            large_image='hammerlauncher',
+            large_text='electrovoyage\'s Hammer Launcher'
+        )
+        print('\t3/3 - Rich Presence set')
+    except DiscordNotFound:
+        print('Discord not found, failed to start Rich Presence')
 
-def GetHammerFilename(win_name: str) -> str:
-    return path.basename(win_name.split('-')[1].strip().split('-')[0].strip()[1:])
+    FOUND, IDLE, MISSING, SEARCHING = range(4)
+    starttimes = {}
 
-while True:
-    hammer_state = None
-    # Get a list of all windows
-    windows_list = gw.getAllWindows()
+    def GetHammerFilename(win_name: str) -> str:
+        return path.basename(win_name.split('-')[1].strip().split('-')[0].strip()[1:])
 
-    # Extract names of all windows
-    window_names: list[str] = [window.title for window in windows_list]
+    while True:
+        if shouldend:
+            break
+        hammer_state = None
+        # Get a list of all windows
+        windows_list = gw.getAllWindows()
 
-    # Print the list of window names
-    #print('Searching for Hammer...')
-    hammer_state = SEARCHING
-    for name in window_names:
-        #print('Hammer - ' in name) 
-        if name.startswith('Hammer - ['):
-            hammer_state = FOUND
+        # Extract names of all windows
+        window_names: list[str] = [window.title for window in windows_list]
 
-            if (getViewportIndependentWindowName(old_hammername) != getViewportIndependentWindowName(name)) and (getViewportIndependentWindowName(name) not in starttimes.keys()):
-                starttimes[getViewportIndependentWindowName(name)] = time.time()
+        # Print the list of window names
+        #print('Searching for Hammer...')
+        hammer_state = SEARCHING
+        for name in window_names:
+            #print('Hammer - ' in name) 
+            if name.startswith('Hammer - ['):
+                hammer_state = FOUND
 
-            old_hammername = hammer_name
-            hammer_name = name
-            #print('Hammer: map is open')
-        elif name == 'Hammer':
-            hammer_state = IDLE
-            #print('Hammer found')
-    if hammer_state == SEARCHING:
-        hammer_state = MISSING
+                if (getViewportIndependentWindowName(old_hammername) != getViewportIndependentWindowName(name)) and (getViewportIndependentWindowName(name) not in starttimes.keys()):
+                    starttimes[getViewportIndependentWindowName(name)] = time.time()
 
-    # Extract the 'path to file' part
-    if hammer_state == FOUND:
-        path_to_file = GetHammerFilename(hammer_name)
-        
-        try:
-            rpc.update(
-                details='Editing a map',
-                state=f'Editing {path_to_file}',
-                start = starttimes[getViewportIndependentWindowName(hammer_name)],
-                large_image='hammerlauncher',
-                large_text='electrovoyage\'s Hammer Launcher'
-            )
-        except DiscordNotFound:
-            print('Discord not found, failed to start Rich Presence')
-        except NameError:
-            print('nameerror')
-            pass
-    elif hammer_state is IDLE:
-        try:
-            rpc.update(
-                details='Idling',
-                state='electrovoyage\'s Hammer Launcher is open',
-                start = starttime,
-                large_image='hammerlauncher',
-                large_text='electrovoyage\'s Hammer Launcher'
-            )
-        except DiscordNotFound:
-            print('Discord not found, failed to start Rich Presence')
+                old_hammername = hammer_name
+                hammer_name = name
+                #print('Hammer: map is open')
+            elif name == 'Hammer':
+                hammer_state = IDLE
+                #print('Hammer found')
+        if hammer_state == SEARCHING:
+            hammer_state = MISSING
+
+        # Extract the 'path to file' part
+        if hammer_state == FOUND:
+            path_to_file = GetHammerFilename(hammer_name)
+
+            try:
+                rpc.update(
+                    details='Editing a map',
+                    state=f'Editing {path_to_file}',
+                    start = starttimes[getViewportIndependentWindowName(hammer_name)],
+                    large_image='hammerlauncher',
+                    large_text='electrovoyage\'s Hammer Launcher'
+                )
+            except DiscordNotFound:
+                print('Discord not found, failed to start Rich Presence')
+            except NameError:
+                print('nameerror')
+                pass
+        elif hammer_state is IDLE:
+            try:
+                rpc.update(
+                    details='Idling',
+                    state='electrovoyage\'s Hammer Launcher is open',
+                    start = starttime,
+                    large_image='hammerlauncher',
+                    large_text='electrovoyage\'s Hammer Launcher'
+                )
+            except DiscordNotFound:
+                print('Discord not found, failed to start Rich Presence')
+              
+def stop():
+    global shouldend
+    shouldend = True
+            
+if __name__ == '__main__':
+    main()
